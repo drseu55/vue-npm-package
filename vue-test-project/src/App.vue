82b78d1c7182f004045d947e27ad64c18f799e5f
cr0c0pk1 <alpics1337@gmail.com>
2021-12-26 12:37:42 +0200

    fix: Fix cached folder

---
+++ vue-test-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <FujiTestnetLogo />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import FujiTestnetLogo from 'fuji-testnet-logo'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    FujiTestnetLogo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
